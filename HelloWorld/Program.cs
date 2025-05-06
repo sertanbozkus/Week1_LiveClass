9f5af8f5d632982c9885c55038e902fde5da52e9
serta <serta@SRTN-NOTEBOOK>
2025-05-06 21:03:03 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Merhaba Dünya");
+Console.WriteLine("Benim adım Sertan");
+Console.WriteLine("Hoşçakal.");
+
+
+Console.WriteLine("---------------------------------");
+
+Console.Write("Merhaba Dünya");
+Console.Write("Benim adım Sertan\n");
+
+
+Console.WriteLine("----------------------------------");
+
+string ad;
+ad = "Ajda Pekkan";
+
+Console.WriteLine(ad);
+
+ad = Console.ReadLine(); // console.readline kullanıcıdan console üzerinden veri ister.
+
+
+Console.WriteLine(ad);
+
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Yaşınız kaç ?");
+int yas = Convert.ToInt32(Console.ReadLine()); // 23
+
+// Convert.ToInt32("234"); --> 234
+
+Console.WriteLine("O halde adınız -> " + ad + " ve yaşınız -> " + yas);
